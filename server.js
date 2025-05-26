fce1114d7dcc0be361294abff8801dc9ca726f8a
LuigiS-R <luigisr03@gmail.com>
2025-05-26 17:05:10 +0900

    Server Update
    -Static serving of frontend files
    -Enabling multiple files uploads for exam papers
    -Enabling answer key upload

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 const axios = require("axios");
+const cheerio = require("cheerio");
 const FormData = require("form-data")
 
+//Function to extract answers from answerKey
+function getAnswers(rawHTML){
+    let answersArray = {};
+    
+    const $ = cheerio.load(rawHTML);
+    const text = $('p').html();
+
+    let unfilteredArray = text.split("<br>");
+
+    for(let i = 0; i<unfilteredArray.length; i++){
+        answersArray[i] = filtering(unfilteredArray[i]);
+    }
+
+    return answersArray;
+
+    function filtering(subString){
+        return subString.substring(subString.indexOf(")")+1);
+    }
+}
+
 const app = express();
-
-//Class to store and handle exam answers by student
-class Student{
-    constructor(name, id, answers){
-        this.name = name;
-        this.id = id;
-        this.answers = answers;
-    }
-}
-
-//Function to getAnswers from rawHTML
-function getAnswers(content){
-    const lines = content.split("<br>");
-    let answers = [];
-
-    for(let i = 0; i<lines.length; i++){
-        if (i % 2 != 0){
-            answers.push(lines[i].trim());
-        }
-    }
-
-    return answers;
-}
 
 app.use( (req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*');
     next();
+})
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
+app.get("/", (req, res)=>{
+
 })
 
 const storage = multer.diskStorage({
@@ -44,42 +49,46 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage});
 
-app.post("/uploads", upload.single("document"), async (req, res) =>{
-    console.log("File has been uploaded successfully!");
+app.post("/uploads", upload.array("documents", 60), async (req, res) =>{
+    console.log("Files have been uploaded successfully!");
 
-    const filePath = req.file.path;
-    const form = new FormData();
-    form.append("document", fs.createReadStream(filePath));
-
-    const response = await axios.post("https://api.upstage.ai/v1/document-ai/document-parse", form, {
-        headers:{
-            Authorization: `Bearer up_zP6g7rXdjCuEx2XGrUT6NECpvHQsP`,
-            ...form.getHeaders()
-        },
-    })
-
-    //Handling Document parser API response (rawHTML)
-        let $ = cheerio.load(response.data.content.html);
-    
-        studentName = $("tbody td").first().html();
-        studentID = $("tbody td").eq(1).html();
-        
-        paragraphContent = $("p").html();
-    
-        studentAnswers = getAnswers(paragraphContent);
-    
-        //Creating an object to store the info from each exam paper
-        let student = new Student(studentName, studentID, studentAnswers);
-    
-        res.json({
-            studentName: student.name,
-            studentID : student.id,
-            answers : student.answers
+    let responseObj = {};
+    let counter = 0;
+    for(const file of req.files) {
+        const form = new FormData();
+        form.append("document", fs.createReadStream(file.path));
+        let response = await axios.post("https://api.upstage.ai/v1/document-ai/document-parse", form, {
+            headers:{
+                Authorization: `Bearer ${process.env.UPSTAGE_API_KEY}`,
+                ...form.getHeaders()
+            },
         })
+        responseObj[counter++] = response.data.content.html;
+    }
+    res.json(responseObj);
+
 })
 
-app.get("/", (req, res)=>{
-    res.json({message: "Hello, World!"});
+const answerKeystorage = multer.diskStorage({
+    destination: "./answerKey",
+    filename: (req, file, cb) =>{
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+})
+
+const answerKeyUpload = multer({storage: answerKeystorage})
+
+app.post("/uploadAnswerKey", answerKeyUpload.single("document"), async (req, res)=>{
+    const form = new FormData();
+    form.append("document", fs.createReadStream(req.file.path));
+    let response = await axios.post("https://api.upstage.ai/v1/document-ai/document-parse", form, {
+        headers:{
+            Authorization: `Bearer ${process.env.UPSTAGE_API_KEY}`,
+            ...form.getHeaders()
+        }
+    })
+    const rawAnswers = response.data.content.html;
+    res.json(getAnswers(rawAnswers));
 })
 
 app.listen(3000, ()=>{
